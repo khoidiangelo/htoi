531b5279138100e1055b0de242ca1d325aea030d
Tim <horochowiktim@pm.me>
2023-07-06 21:44:59 +0200

    fully functional

--- src/htoi.c
+++ src/htoi.c
@@ -1,25 +1,25 @@
 #include <math.h>
 #include <ctype.h>
-unsigned htoi(char s[], int length) {
+unsigned int htoi(char s[], int length) {
     unsigned int hexadecimal = 0, i = 0;
     if (s[0] == '0' && tolower(s[1]) == 'x') {
         i = 2;
     }
     if (s[length-1] == '\n') {
-        length -= 1;
+        --length;
     }
     for (;i < length; ++i) {
         if (s[i] == '1' && s[i+1] == '4') {
-            hexadecimal+= 20*(int)(pow(16.0,(double)length-i-2));
-            i+=2;
+            hexadecimal += 20*(int)(pow(16.0,(double)length-i-2));
+            ++i;
         } else if (s[i] == '3' && tolower(s[i+1]) == 'f') {
-            hexadecimal+= 63*(int)(pow(16.0,(double)length-i-2));
-            i+=2;
+            hexadecimal += 63*(int)(pow(16.0,(double)length-i-2));
+            ++i;
         } else {
             if (isdigit(s[i])) {
                 hexadecimal += (s[i]-'0')*(int)(pow(16.0,(double)length-i-1));
-            } else {    // 97 -> 103 a -> f | -87 equals to int value needed for hex
-                hexadecimal += (s[i]-87)*(int)(pow(16.0,(double)length-i-1));
+            } else {    // 97 -> 103 a -> f | -87 equals to int value needed for iex
+                hexadecimal += (tolower(s[i])-87)*(int)(pow(16.0,(double)length-i-1));
             }
         }
     }
