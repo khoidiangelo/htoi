e700261c1bbf994a975a6e8a018994b6327d6331
Tim <horochowiktim@pm.me>
2023-07-06 16:17:09 +0200

    POC

--- src/htoi.c
+++ src/htoi.c
@@ -1,21 +1,23 @@
 #include <math.h>
 #include <ctype.h>
-// 97 -> 103 a -> f
-// TODO: FOR UPPER CASE USE LOWER FUNCTION IN C !!!
 int htoi(char s[], int length) {
-    signed int until, hexadecimal = 0;
-    if (s[0] == '0' && (s[1] == 'x' || s[1] == 'X')) {
-        until = length-2;
+    signed int until, hexadecimal = 0, i = 0;
+    if (s[0] == '0' && tolower(s[1]) == 'x') {
+        i = 2;
     }
     if (s[length] == '\n') {
         until = length-1;
     }
-    for (int i = 0; i < length; ++i) {
-        if (isdigit(s[length-i])) {
-            hexadecimal += (int)(s[length-i]-'0'*(pow(16.0,(double)i)));
+    for (;i < length; ++i) {
+        if (s[length-1] == '4' || tolower(s[length-1]) == 'e') {
+            // TODO: special case for 14 and 3f
         } else {
-
+            if (isdigit(s[length-i])) {
+                hexadecimal += (int)((s[length-i]-'0')*(pow(16.0,(double)i)));
+            } else {    // 97 -> 103 a -> f | -87 equals to int value needed for hex
+                hexadecimal += (int)((s[length-i]-87)*(pow(16.0,(double)i)));
+            }
         }
     }
-    return 0;
+    return hexadecimal;
 }
--- src/main.c
+++ src/main.c
@@ -6,6 +6,6 @@
 int main() {
     char s[LENGTH];
     int length = readline(s, LENGTH);
-    printf("%s\n%d", s, htoi(s, length));
+    printf("%s\n%d\n", s, htoi(s, length));
     return 0;
 }
