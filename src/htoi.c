ee88856e99c16d544f4598deff539e54f72d591a
Tim <horochowiktim@pm.me>
2023-07-06 18:53:41 +0200

    special case WIP

--- src/htoi.c
+++ src/htoi.c
@@ -1,6 +1,5 @@
 #include <math.h>
 #include <ctype.h>
-#include <stdio.h>
 unsigned htoi(char s[], int length) {
     unsigned int hexadecimal = 0, i = 0;
     if (s[0] == '0' && tolower(s[1]) == 'x') {
@@ -10,13 +9,17 @@ unsigned htoi(char s[], int length) {
         length -= 1;
     }
     for (;i < length; ++i) {
-        if (s[i] == '1' || s[i] == '3') {
-            // TODO: special case for 14 and 3f
+        if (s[i] == '1' && s[i+1] == '4') {
+            hexadecimal+= 20*(int)(pow(16.0,(double)length-i-2));
+            i+=2;
+        } else if (s[i] == '3' && tolower(s[i+1]) == 'f') {
+            hexadecimal+= 63*(int)(pow(16.0,(double)length-i-2));
+            i+=2;
         } else {
             if (isdigit(s[i])) {
-                hexadecimal += ((s[i]-'0')*(int)(pow(16.0,(double)length-i-1)));
+                hexadecimal += (s[i]-'0')*(int)(pow(16.0,(double)length-i-1));
             } else {    // 97 -> 103 a -> f | -87 equals to int value needed for hex
-                hexadecimal += ((s[i]-87)*(int)(pow(16.0,(double)length-i-1)));
+                hexadecimal += (s[i]-87)*(int)(pow(16.0,(double)length-i-1));
             }
         }
     }
